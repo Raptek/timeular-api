cd4c6ad34deb5f3612951dbf440121ca757e7cc9
Krzysztof Winiarski <krzysztof.winiarski@raptek.pl>
2024-03-06 22:34:09 +0100

    6th day of challenge
    - initialize tests

---
+++ tests/unit/Serializer/JsonSerializerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Timeular\Serializer;
+
+use PHPUnit\Framework\Attributes\Test;
+use Timeular\Serializer\JsonSerializer;
+use PHPUnit\Framework\TestCase;
+
+class JsonSerializerTest extends TestCase
+{
+    #[Test]
+    public function testItWorks(): void
+    {
+        self::assertTrue(true);
+    }
+}
