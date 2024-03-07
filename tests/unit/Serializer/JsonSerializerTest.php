d45c09aaaf656ac1a15bda107f9f3f4459992d0c
Krzysztof Winiarski <krzysztof.winiarski@raptek.pl>
2024-03-07 18:56:56 +0100

    7th day of challenge
    - add xdebug

--- tests/unit/Serializer/JsonSerializerTest.php
+++ tests/unit/Serializer/JsonSerializerTest.php
@@ -2,16 +2,15 @@
 
 declare(strict_types=1);
 
-namespace Tests\Timeular\Serializer;
+namespace Tests\Unit\Timeular\Serializer;
 
 use PHPUnit\Framework\Attributes\Test;
-use Timeular\Serializer\JsonSerializer;
 use PHPUnit\Framework\TestCase;
 
 class JsonSerializerTest extends TestCase
 {
     #[Test]
-    public function testItWorks(): void
+    public function it_works(): void
     {
         self::assertTrue(true);
     }
