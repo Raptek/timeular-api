8ad2c91a1b864f8aa8d8bc5836f578a6a0f45eab
Krzysztof Winiarski <krzysztof.winiarski@raptek.pl>
2024-04-14 19:59:56 +0200

    45th day of challenge
    - kill mutants
    
    - update dependencies

--- tests/unit/Model/UserProfile/SpaceTest.php
+++ tests/unit/Model/UserProfile/SpaceTest.php
@@ -8,28 +8,45 @@ use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Timeular\Exception\MissingArrayKeyException;
+use Timeular\Model\UserProfile\RetiredUser;
+use Timeular\Model\UserProfile\Role;
 use Timeular\Model\UserProfile\Space;
+use Timeular\Model\UserProfile\User;
 
 class SpaceTest extends TestCase
 {
     #[Test]
     public function it_creates_space_from_array():void
     {
+        $member = [
+            'id' => '1',
+            'name' => 'my name',
+            'email' => 'my-name@example.com',
+            'role' => Role::Admin->value,
+        ];
+        $retiredMember = [
+            'id' => '1',
+            'name' => 'my name',
+        ];
         $space = Space::fromArray(
             [
                 'id' => '1',
                 'name' => 'My Personal Space',
                 'default' => $default = (bool)rand(0, 1),
-                'members' => [],
-                'retiredMembers' => [],
+                'members' => [
+                    $member,
+                ],
+                'retiredMembers' => [
+                    $retiredMember,
+                ],
             ]
         );
 
         self::assertEquals('1', $space->id);
         self::assertEquals('My Personal Space', $space->name);
         self::assertEquals($default, $space->default);
-        self::assertEquals([], $space->members);
-        self::assertEquals([], $space->retiredMembers);
+        self::assertEquals([User::fromArray($member)], $space->members);
+        self::assertEquals([RetiredUser::fromArray($retiredMember)], $space->retiredMembers);
     }
 
     #[Test]
@@ -45,12 +62,26 @@ class SpaceTest extends TestCase
     #[Test]
     public function it_converts_to_array(): void
     {
+        $member = [
+            'id' => '1',
+            'name' => 'my name',
+            'email' => 'my-name@example.com',
+            'role' => Role::Admin->value,
+        ];
+        $retiredMember = [
+            'id' => '1',
+            'name' => 'my name',
+        ];
         $data = [
             'id' => '1',
             'name' => 'My Personal Space',
-            'default' => $default = (bool)rand(0, 1),
-            'members' => [],
-            'retiredMembers' => [],
+            'default' => (bool)rand(0, 1),
+            'members' => [
+                $member,
+            ],
+            'retiredMembers' => [
+                $retiredMember,
+            ],
         ];
 
         $user = Space::fromArray($data);
